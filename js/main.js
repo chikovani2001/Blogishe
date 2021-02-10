3203e9aad92728422ae9a08e08aacb56e4d76e4b
chikovani2001 <chikovani2001@inbox.ru>
2021-02-10 20:09:53 +0400

    message

--- js/main.js
+++ js/main.js
@@ -6,28 +6,31 @@ setNavigation();
 
 function setNavigation() {
   fetch("GET", "/navigation").then((navigation) => {
-    navigation.forEach((element) => {
-      if (!element.guard) {
-        const li = document.createElement("li");
-        li.classList.add("nav-item");
-        const a = document.createElement("a");
-        a.classList.add("nav-link");
-        a.setAttribute("href", element.url);
-        a.setAttribute("id", element.idName);
-        a.textContent = element.name;
-        li.append(a);
-        nav.append(li);
-      } else if (isAuthorithed() && element.guard && element.auth === "show") {
-        const li = document.createElement("li");
-        li.classList.add("nav-item");
-        const a = document.createElement("a");
-        a.classList.add("nav-link");
-        a.setAttribute("href", element.url);
-        a.setAttribute("id", element.idName);
-        a.textContent = element.name;
-        li.append(a);
-        nav.append(li);
-      }
+    let menuNavigation = navigation.guest;
+    if(isAuthorithed()){
+      menuNavigation = navigation.user;
+    }
+      
+    menuNavigation.forEach((element) => {
+      const li = document.createElement("li");
+      li.classList.add("nav-item");
+      const a = document.createElement("a");
+      a.classList.add("nav-link");
+      a.setAttribute("href", element.url);
+      a.setAttribute("id", element.idName);
+      a.textContent = element.name;
+      li.append(a);
+      nav.append(li);  
+      
+      
+      a.addEventListener("click", (e) => {
+        if(e.target.id === "logout"){
+          e.preventDefault();
+          localStorage.removeItem("isAuthorized")
+          window.location = "/"
+        }
+        
+      })
     });
   });
 }
